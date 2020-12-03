4a7cb05355aa5df8023ce4fdb3a301f74ff202e0
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2020-12-03 09:58:40 +0800

    Python 3 compatible and change to PAT authorization

--- github_unfollower.py
+++ github_unfollower.py
@@ -3,35 +3,28 @@ from github import Github
 from github.GithubException import GithubException
 from getpass import getpass
 
-if __name__ == "__main__":
-    gitHubUsername = raw_input("Please enter your Github username: ").strip()
-    gitHubPassword = getpass("Please enter your Github password: ").strip()
+if __name__ == '__main__':
+    gitHubPAT = getpass('Please enter your Github personal access token: ').strip()
     
-    gitUser = Github(gitHubUsername, gitHubPassword)
+    gitUser = Github(gitHubPAT)
     
     following_count = 0
     followers_count = 0
 
     try:
-        gitHubName = gitUser.get_user().name
-        gitHubId = gitUser.get_user().id
-        followers_count = gitUser.get_user().followers
-        following_count = gitUser.get_user().following
-
-        print "Your Github ID is: %s\nYour Github Name is: %s" % (gitHubId, gitHubName)
-        print "You got %s followers and following %s Github users..." % (followers_count, following_count)
+        print('Your Github ID is: {}\nYour Github Name is: {}'.format(gitUser.get_user().id, gitUser.get_user().name))
+        print('You got {} followers and following {} Github users...'.format(gitUser.get_user().followers, gitUser.get_user().following))
     except GithubException as e:
-        print "This script can't connect to Github with following information:\n\nUsername: %s\nPassword: %s" % (gitHubUsername,gitHubPassword)
-        print "Run this script again if you want to use it... :)"
+        print('This script can\'t connect to Github with following information:\n\Token: {}'.format(gitHubPAT))
+        print('Run this script again if you want to use it... :)')
         exit()
-
 
     followers = [follower.id for follower in gitUser.get_user().get_followers()] 
 
-    print "\n\n"
+    print('\n\n')
 
     for following in gitUser.get_user().get_following():
-        print "Checking Github ID: %s and Github Name: %s" % (following.id, following.name)
+        print('Checking Github ID: {} and Github Name: {}'.format(following.id, following.name))
         if following.id not in followers:
             gitUser.get_user().remove_from_following(following)
-            print "Github user with ID: %s and Name: %s is not following you and automaticaly unfollowed by this script" % (following.id, following.name)
+            print('Github user with ID: {} and Name: {} is not following you and automaticaly unfollowed by this script'.format(following.id, following.name))
